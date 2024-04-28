5a82af7881bd06bfd4a9fc95c49e8f9ff9583a08
Masudur400 <md8004210@gmail.com>
2024-04-28 18:35:41 +0600

    last update

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ const port = process.env.PORT || 5000;
 
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
 
-
-
-
+ 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nhw8ipw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
+
+ 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
